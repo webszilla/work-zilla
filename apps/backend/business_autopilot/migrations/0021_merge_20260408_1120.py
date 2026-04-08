176b79e9dda04ef07a8742b830a4356ea1943987
guru <apple@MacBook-Pro.local>
2026-04-08 09:16:15 +0530

    Add business autopilot migration merge

---
+++ apps/backend/business_autopilot/migrations/0021_merge_20260408_1120.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("business_autopilot", "0020_crm_reference_id_flow"),
+        ("business_autopilot", "0020_crmsalesorder_payment_fields"),
+    ]
+
+    operations = []
